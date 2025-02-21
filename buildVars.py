956df47a241ef55dbbcb2aa918b72fd861921901
gera-ar <delsur@live.com.ar>
2025-02-21 05:45:41 -0300

    github account changed

--- buildVars.py
+++ buildVars.py
@@ -29,7 +29,7 @@ addon_info = {
 	# Author(s)
 	"addon_author": "Gerardo Kessler <gera.ar@yahoo.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/GerardKessler/whatsapp",
+	"addon_url": "https://github.com/gera-ar/whatsapp",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
